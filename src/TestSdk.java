f1eaba4d0630ed288310ac683f4402a4495580d0
GaoYan <yan.gao@vhall.com>
2017-11-14 17:11:37 +0800

    添加注册，更新， 本地头像支持，添加 数据统计 currentData currentAudience 支持

--- src/TestSdk.java
+++ src/TestSdk.java
@@ -10,9 +10,9 @@ public class TestSdk {
     public static void main(String[] args)
     {
         HashMap<String, Object> configMap = new HashMap<String, Object>();
-        configMap.put("app_key", "xxxxxxxxxx"); // app_key 请像技服人员申请
-        configMap.put("secret_key", "xxxxxxxxxx");  // secret_key 请像技服人员申请
-        configMap.put("request_domain", "http://e.vhall.com");
+        configMap.put("app_key", "dc16c76acfd9b0a82fb3f3039d7bcfe7"); // app_key 请像技服人员申请
+        configMap.put("secret_key", "1052ffd2b5c33283e90093dbbe706607");  // secret_key 请像技服人员申请
+        configMap.put("request_domain", "http://t.e.vhall.com");
 
         Webinar webinarObj = new Webinar(configMap);
         User userObj = new User(configMap);
@@ -21,32 +21,46 @@ public class TestSdk {
         HashMap<String, Object> paramActiveImage = new HashMap<String, Object>();
         HashMap<String, Object> paramFetch = new HashMap<String, Object>();
         HashMap<String, Object> paramList = new HashMap<>();
-        HashMap<String, Object> paramCreateUser = new HashMap<>();
+        HashMap<String, Object> paramCreateUser = new HashMap<String, Object>();
+        HashMap<String, Object> paramCurrent = new HashMap<String, Object>();
 
-        paramCreate.put("start_time", webinarObj.toString(new Date().getTime()).substring(0,10)); // 时间固定格式为10位长度时间戳
-        paramCreate.put("subject", "take a test again");
-        paramCreateUser.put("third_user_id", "vhall_sdk_test");
-         paramCreateUser.put("pass", "gaoyansing");
+        paramCreate.put("start_time", "123456789");
+        paramCreate.put("subject", "take a test");
+
+        paramCreateUser.put("third_user_id", "third_sdk_test_2");
+        paramCreateUser.put("pass", "gaoyansing");
+        paramCreateUser.put("local_head", new File("").getAbsolutePath() + "/resources/vhall.png");
+        paramCurrent.put("begintime", "2016-7-12 10:12:11");
+        paramCurrent.put("endtime", "2017-5-12 11:12:11");
+        paramCurrent.put("user_id", "461");
+
 
         try {
-            String resultList = webinarObj.list(paramList);
-            webinarObj.dump(resultList);
+            String resultList = userObj.register(paramCreateUser);
+            String result = userObj.update(paramCreateUser);
+            userObj.dump(resultList);
+            userObj.dump(result);
+
+            String concurrentAudience = userObj.concurrentAudience(paramCurrent);
+            String concurrentDate = userObj.concurrentDate(paramCurrent);
+            userObj.dump(concurrentAudience);
+            userObj.dump(concurrentDate);
 
             String resultCreateUser = userObj.register(paramCreateUser);
             webinarObj.dump(resultCreateUser);
 
             String resultCreate = webinarObj.create(paramCreate);
-            // 请使用绝对路径获取文件
+//            // 请使用绝对路径获取文件
             paramActiveImage.put("image", new File("").getAbsolutePath() + "/resources/vhall.png");
             paramActiveImage.put("webinar_id", JsonPath.read(resultCreate, "$.data"));
-
+//
             String resultActiveImage = webinarObj.activeimage(paramActiveImage);
-
+//
             paramFetch.put("webinar_id", JsonPath.read(resultCreate, "$.data"));
             paramFetch.put("fields", "subject, start_time");
-
+//
             String resultFetch = webinarObj.fetch(paramFetch);
-
+//
             webinarObj.dump(JsonPath.read(resultCreate, "$.data"));
             webinarObj.dump(JsonPath.read(resultActiveImage, "$.data"));
             webinarObj.dump(JsonPath.read(resultFetch, "$.data"));
